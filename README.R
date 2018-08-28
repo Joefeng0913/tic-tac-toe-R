2ae55bc86d1136c6e57ff8b5c52df0f14cac08dd
Zicong Feng <Joefeng@Zicongs-MacBook-Pro.local>
2018-08-27 22:57:39 -0700

    First commit

---
+++ README.R
@@ -0,0 +1,9 @@
+
+
+
+# tic-tac-toe implementation in R 
+
+**This small project implememted the _tic-tac-toe_ board game using R programming language.**
+> The game is able to detect when a player has won. As soon as a player wins, the game will stop. 
+> It should show the final state of the board and announce who won, eg. "X wins"
+
